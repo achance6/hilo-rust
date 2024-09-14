b357f2a3084d22985e7e37321ed74afdbc0f720c
Ayden Chance <achance6@jhu.edu>
2024-09-14 19:43:10 -0400

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use rand;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let target = rand::thread_rng().gen_range(1..=100);
+
+    println!("The target number is {target}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Line should be read successfully");
+
+    println!("You guessed: {}", guess);
 }
